3869f39671d00a25021b6897b568a72dbd46b0a8
karimabbes <karim.abbes@tum.de>
2021-03-01 11:35:25 +0100

    dd

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -21,13 +21,17 @@ resource "openstack_networking_secgroup_v2" "k8s_master_extra" {
   count                = "%{if var.extra_sec_groups}1%{else}0%{endif}"
   name                 = "${var.cluster_name}-k8s-master-${var.extra_sec_groups_name}"
   description          = "${var.cluster_name} - Kubernetes Master nodes - rules not managed by terraform"
-  direction            = "ingress"
-  ethertype            = "IPv4"
-  protocol             = "tcp"
-  port_range_min       = "22"
-  port_range_max       = "22"
-  remote_ip_prefix     = openstack_networking_secgroup_v2.k8s_master_extra.id
   delete_default_rules = true
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_master_extra" {
+  count            = "%{if var.extra_sec_groups}1%{else}0%{endif}"
+  direction        = "ingress"
+  ethertype        = "IPv4"
+  protocol         = "tcp"
+  port_range_min   = "22"
+  port_range_max   = "22"
+  remote_ip_prefix = openstack_networking_secgroup_v2.k8s_master_extra.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "k8s_master" {
@@ -79,7 +83,7 @@ resource "openstack_networking_secgroup_v2" "k8s" {
 resource "openstack_networking_secgroup_rule_v2" "k8s" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  remote_group_id   = openstack_networGIking_secgroup_v2.k8s.id
+  remote_group_id   = openstack_networking_secgroup_v2.k8s.id
   security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 
