56564924675a0e84c0b1102c165dc1ad04a9b43a
karimabbes <karim.abbes@tum.de>
2021-03-01 12:03:59 +0100

    dcd

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -84,7 +84,7 @@ resource "openstack_networking_secgroup_rule_v2" "k8s_ssh" {
   port_range_min    = "22"
   port_range_max    = "22"
   remote_group_id   = ""
-  security_group_id = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "k8s_allowed_remote_ips" {
