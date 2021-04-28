3600e27cfd3b59670de8b3e02621131ee7e66f2c
karimabbes <karim.abbes@tum.de>
2021-04-28 11:24:44 +0100

    sdfsdf

--- scripts/configure_kubectl.sh
+++ scripts/configure_kubectl.sh
@@ -8,12 +8,13 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 mkdir ~/.kube/
+mkdir k8s_keys
 ssh ubuntu@$1 sudo cat /etc/kubernetes/kubelet.conf > ~/.kube/config
-ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/apiserver-kubelet-client.key > admin.key
-ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/apiserver-kubelet-client.crt > admin.crt
-ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/ca.crt > ca.crt
-kubectl config set-cluster default-cluster --server=https://$1:6443 --certificate-authority=ca.crt
-kubectl config set-credentials default-admin --certificate-authority=ca.crt --client-key=admin.key --client-certificate=admin.crt
+ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/apiserver-kubelet-client.key > k8s_keys/admin.key
+ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/apiserver-kubelet-client.crt > k8s_keys/admin.crt
+ssh ubuntu@$1 sudo cat /etc/kubernetes/ssl/ca.crt > k8s_keys/ca.crt
+kubectl config set-cluster default-cluster --server=https://$1:6443 --certificate-authority=k8s_keys/ca.crt
+kubectl config set-credentials default-admin --certificate-authority=k8s_keys/ca.crt --client-key=k8s_keys/admin.key --client-certificate=k8s_keys/admin.crt
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin
 kubectl config use-context default-system
 kubectl version
