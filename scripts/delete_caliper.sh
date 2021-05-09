e84d3683a63fe3a945f7843c5cae3e57faf75be7
karimabbes <karim.abbes@tum.de>
2021-05-09 14:50:01 +0100

    sdf

--- scripts/delete_caliper.sh
+++ scripts/delete_caliper.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+set -x
 kubectl delete jobs caliper
 kubectl delete jobs caliper-worker
 kubectl delete configmap  benchmarks
 kubectl delete configmap network
 kubectl delete configmap workload
 kubectl delete configmap caliper-config  
+
 kubectl delete deployment mosquitto
 kubectl delete svc mosquitto
-argo delete --all
-helm delete hlf-kube
+set +x
---
+++ scripts/run_caliper.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -x
+
+if [[ $# -eq 0 ]] ; then
+    echo 'chaincode name is missing'
+    exit 0
+fi
+
+# Go to benchmarking folder
+cd `dirname $0`/../benchmarking
+
+kubectl apply -f benchmarking/mosquitto/
+
+kubectl create configmap benchmarks --from-file=./$1/benchmarks/
+
+kubectl create configmap workload --from-file=./$1/workload/
+
+kubectl create configmap network --from-file=./networks/
+
+kubectl create configmap caliper-config --from-file=./caliper.yaml
+
+kubectl apply -f caliper-deployment.yaml 
+
+kubectl apply -f caliper-deployment-worker.yaml 
+
+kubectl get pods --output=wide -w
+
+set +x
