4d1194606e53de18f7672d342f1ccad0992c0c53
karimabbes <karim.abbes@tum.de>
2021-05-16 16:34:57 +0100

    sf

--- scripts/caliper_run.sh
+++ scripts/caliper_run.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 set -x
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Chaincode name is missing'
+if [[ $# -l 3 ]] ; then
+    echo '2 arguments are expected'
     exit 0
 fi
+
+CHAINCODE_NAME="$1"
+NETWORK_FOLDER_NAME="$2"
 
 # Go to benchmarking folder
 cd `dirname $0`/../benchmarking
 
 kubectl apply -f mosquitto/
 
-kubectl create configmap benchmarks --from-file=./$1/benchmarks/
+kubectl create configmap benchmarks --from-file=./$CHAINCODE_NAME/benchmarks/
 
-kubectl create configmap workload --from-file=./$1/workload/
+kubectl create configmap workload --from-file=./$CHAINCODE_NAME/workload/
 
-kubectl create configmap network --from-file=./networks/
+kubectl create configmap network --from-file=./networks/$NETWORK_FOLDER_NAME
 
 kubectl create configmap caliper-config --from-file=./caliper.yaml
 
