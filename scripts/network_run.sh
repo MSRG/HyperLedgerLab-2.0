8a7389b01d31d51e0051baf7a9afef1905dc9aa7
karimabbes <karim.abbes@tum.de>
2021-05-24 13:10:26 +0200

    dgdfg

--- scripts/network_run.sh
+++ scripts/network_run.sh
@@ -23,19 +23,10 @@ helm install hlf-kube ./hlf-kube/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/c
 
 helm upgrade hlf-kube ./hlf-kube/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/crypto-config.yaml -f $FOLDER_NAME/hostAliases.yaml 
 
+# Check if pods exist and running
+# we don't check for CA because if peers and orderers are running then CA pods are also running. 
 echo "Wait until orderer pods are all running..."
 while [[ $(kubectl get pods -l name=hlf-orderer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == *"False"* ]]; do echo "waiting for orderer pods" && sleep 1; done
-OUTPUT=$(kubectl get  pods -l name=hlf-orderer)
-echo "====="
-echo "${OUTPUT}"
-echo "====="
-
-MULTILINE=$(ls \
-   -1)
-   echo "====="
-
-echo "${MULTILINE}"
-echo "====="
 
 if [ -z $(kubectl get  pods -l name=hlf-orderer) ] ; then 
     echo 'Orderer pods does not exist. Please check the error.'
@@ -45,15 +36,10 @@ fi
 echo "Wait until peer pods are all running..."
 while [[ $(kubectl get pods -l name=hlf-peer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == *"False"* ]]; do echo "waiting for peer pods" && sleep 1; done
 
-if [[ $(kubectl get  pods -l name=hlf-peer) == *"No resources found in"* ]] ; then 
+if [ -z $(kubectl get  pods -l name=hlf-peer) ] ; then 
     echo 'Peer pods does not exist. Please check the error.'
     exit 0
 fi 
-
-# kubectl wait pods -l name=hlf-orderer --for=condition=Ready=true
-#  kubectl get  pods -l name=hlf-orderer
- 
-# we don't check for CA because if peers and orderers are running then CA pods are also running. 
 
 echo "Run channel flow..."
 helm template channel-flow/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/crypto-config.yaml -f $FOLDER_NAME/hostAliases.yaml | argo submit - --watch
