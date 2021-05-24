a07051eee3b103f7d0dd57a2b42d77c1e351bf26
karimabbes <karim.abbes@tum.de>
2021-05-24 12:58:05 +0200

    sdf

--- scripts/network_run.sh
+++ scripts/network_run.sh
@@ -25,7 +25,7 @@ helm upgrade hlf-kube ./hlf-kube/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/c
 
 echo "Wait until orderer pods are all running..."
 while [[ $(kubectl get pods -l name=hlf-orderer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == *"False"* ]]; do echo "waiting for orderer pods" && sleep 1; done
-string= $(kubectl get  pods -l name=hlf-orderer)
+string= kubectl get  pods -l name=hlf-orderer
 echo "====="
 echo $string
 echo "====="
