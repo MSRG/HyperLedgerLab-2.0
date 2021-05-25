45d0976bc51cdca4fcf43741be1a3d1099abfe4b
karimabbes <karim.abbes@tum.de>
2021-05-25 14:24:12 +0200

    ghj

--- scripts/network_run.sh
+++ scripts/network_run.sh
@@ -26,22 +26,24 @@ helm upgrade hlf-kube ./hlf-kube/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/c
 # Check if pods exist and running
 # we don't check for CA because if peers and orderers are running then CA pods are also running. 
 echo "Wait until orderer pods are all running..."
-while [[ $(kubectl get pods -l name=hlf-orderer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == *"False"* ]]; do echo "waiting for orderer pods" && sleep 1; done
+ORDERER_STATUS=$(kubectl get pods -l name=hlf-orderer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
+while [[  "${ORDERER_STATUS}" == *"False"* ]] || [[ -z "${ORDERER_STATUS}" ]] ; do echo "waiting for orderer pods" && sleep 1; done
 
-ORDERER=$(kubectl get  pods -l name=hlf-orderer)
-if [ -z "${ORDERER}" ] ; then 
-    echo 'Orderer pods does not exist. Please check the error.'
-    exit 0
-fi 
+# ORDERER=$(kubectl get  pods -l name=hlf-orderer)
+# if [ -z "${ORDERER}" ] ; then 
+#     echo 'Orderer pods does not exist. Please check the error.'
+#     exit 0
+# fi 
 
 echo "Wait until peer pods are all running..."
-while [[ $(kubectl get pods -l name=hlf-peer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == *"False"* ]]; do echo "waiting for peer pods" && sleep 1; done
+PEER_STATUS=$(kubectl get pods -l name=hlf-peer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
+while [[ "${PEER_STATUS}" == *"False"* ]] || [[ -z "${PEER_STATUS}" ]] ; do echo "waiting for peer pods" && sleep 1; done
 
-PEER=$(kubectl get  pods -l name=hlf-peer)
-if [ -z "${PEER}" ] ; then 
-    echo 'Peer pods does not exist. Please check the error.'
-    exit 0
-fi 
+# PEER=$(kubectl get  pods -l name=hlf-peer)
+# if [ -z "${PEER}" ] ; then 
+#     echo 'Peer pods does not exist. Please check the error.'
+#     exit 0
+# fi 
 
 echo "Run channel flow..."
 helm template channel-flow/ -f $FOLDER_NAME/network.yaml -f $FOLDER_NAME/crypto-config.yaml -f $FOLDER_NAME/hostAliases.yaml | argo submit - --watch
