3d8c2c41a959923dbae2502bea79a015a0e2f928
karimabbes <karim.abbes@tum.de>
2021-05-26 14:27:08 +0200

    sdf

--- hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
+++ hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# this script collects peer addresses used with peer lifecycle chaincode commit. 
+# This command needs to target the peers of other organizations on the channel to collect their organization endorsement for the definition.
+if test "$#" -ne 4; then
+   echo "usage: collect_peer_addresses.sh <network.yaml> <crypto_config.yaml> <chaincode_name> <peer>" 
+   exit 2
+fi
+
 
 # exit when any command fails
 set -e
@@ -9,9 +16,6 @@ network_yaml=$1
 crypto_config_yaml=$2
 chaincode=$3
 peer=$4
-
-echo $chaincode
-echo $peer
 
 output=""
 orgIDs=$(yq eval '.network | .chaincodes[] | select (.name == "'$(echo $chaincode)'") | .orgs[]' "$network_yaml")
