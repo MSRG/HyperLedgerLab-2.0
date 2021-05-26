adc1be6797073dc6ed5e7d575c6efd10132804a7
karimabbes <karim.abbes@tum.de>
2021-05-26 10:53:34 +0200

    sdf

--- hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
+++ hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
@@ -3,11 +3,29 @@
 
 # exit when any command fails
 set -e
-set -x
+# set -x
 
+network_yaml=$1
+crypto_config_yaml=$2
+chaincode=$3
+
+echo $(cat $network_yaml) 
 echo $(yq --version)
-anchor_peers=$(yq eval -j '.Organizations[] | select (.Name == "'$(echo $orgID)'") | .AnchorPeers' "$configtx_yaml")
+output=""
+echo $chaincode
+orgIDs=$(yq eval '.network | .chaincodes[] | select (.name == "'$(echo $chaincode)'") | .orgs[]' "$network_yaml")
+echo $orgIDs    
+array=($orgIDs)
 
+for f in "${array[@]}"
+do
+	echo  "--- $f ---  "
+done
 
+orgID="Org1"
 
-set +x
+domain=$(yq eval '.PeerOrgs[] | select (.Name == "'$(echo $orgID)'") | .Domain' "$crypto_config_yaml")
+output="$output --peerAddresses $domain --tlsRootCertFiles /etc/hyperledger/fabric/tls/$domain/ca.crt"
+echo $domain    
+echo $output
+# set +x
