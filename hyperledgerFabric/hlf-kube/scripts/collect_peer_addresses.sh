8439d7f11767aa8d8c465ebaa8f170b2d9688199
karimabbes <karim.abbes@tum.de>
2021-05-26 14:02:03 +0200

    fg

--- hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
+++ hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
@@ -7,27 +7,22 @@ set -e
 
 network_yaml=$1
 crypto_config_yaml=$2
-peer=$3
+chaincode=$3
+peer=$4
 
-set -x
-echo "==================="
-
-echo $network_yaml
-echo $crypto_config_yaml
+echo $chaincode
 echo $peer
 
-echo "==================="
+output=""
+orgIDs=$(yq eval '.network | .chaincodes[] | select (.name == "'$(echo $chaincode)'") | .orgs[]' "$network_yaml")
+array=($orgIDs)
 
-# output=""
-# orgIDs=$(yq eval '.network | .chaincodes[] | select (.name == "'$(echo $chaincode)'") | .orgs[]' "$network_yaml")
-# array=($orgIDs)
-
-# for orgID in "${array[@]}"
-# do
-# domain=$(yq eval '.PeerOrgs[] | select (.Name == "'$(echo $orgID)'") | .Domain' "$crypto_config_yaml")
-# output="$output --peerAddresses $peer.$domain:7051 --tlsRootCertFiles /etc/hyperledger/fabric/tls/$domain/ca.crt"
-# done
+for orgID in "${array[@]}"
+do
+domain=$(yq eval '.PeerOrgs[] | select (.Name == "'$(echo $orgID)'") | .Domain' "$crypto_config_yaml")
+output="$output --peerAddresses $peer.$domain:7051 --tlsRootCertFiles /etc/hyperledger/fabric/tls/$domain/ca.crt"
+done
  
-# echo $output
-set +x
+echo $output
+# set +x
 
