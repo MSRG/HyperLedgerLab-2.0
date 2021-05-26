8c24fd15653e33d3b012511e59257a537466f2a3
karimabbes <karim.abbes@tum.de>
2021-05-26 09:43:04 +0200

    geg

--- hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
+++ hyperledgerFabric/hlf-kube/scripts/collect_peer_addresses.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+
+# exit when any command fails
+set -e
+set -x
+
+echo $(yq --version)
+anchor_peers=$(yq eval -j '.Organizations[] | select (.Name == "'$(echo $orgID)'") | .AnchorPeers' "$configtx_yaml")
+
+
+
+set +x
