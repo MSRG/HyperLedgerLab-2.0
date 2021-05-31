d162b2749ef878e10c19e5dc1ceee5a8c9cab602
karimabbes <karim.abbes@tum.de>
2021-05-31 11:39:46 +0200

    df

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -37,6 +37,16 @@ sudo apt-get install --yes python3-pip
 sudo pip3 install -r kubespray/requirements.txt
 set +x
 
+if compgen -c | grep -q "^pip3" >/dev/null; 
+    then
+        echo -e "##################################################################"
+#     else
+#         curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#         curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#         echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+#         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+fi
+
 #Install Ansible
 if dpkg --get-selections | grep -q "^ansible[[:space:]]*install$" >/dev/null; 
     then
