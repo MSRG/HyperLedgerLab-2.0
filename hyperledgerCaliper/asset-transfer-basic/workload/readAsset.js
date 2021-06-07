f0552742ba514f3f7f58b63bb375ff494756cfd8
karimabbes <karim.abbes@tum.de>
2021-06-07 09:42:20 +0200

    jj

--- hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
+++ hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
@@ -7,24 +7,24 @@ class MyWorkload extends WorkloadModuleBase {
         super();
     }
 
-    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
-        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
-        console.log(this.roundIndex);
-        console.log(this.roundArguments);
-        for (let i = 0; i < this.roundArguments.assets; i++) {
-            const assetID = `${this.workerIndex}_${i}`;
-            console.log(`Worker ${this.workerIndex}: Creating asset ${assetID}`);
-            const request = {
-                contractId: this.roundArguments.contractId,
-                contractFunction: 'CreateAsset',
-                invokerIdentity: 'client0.org1.example.com',
-                contractArguments: [assetID, 'blue', '20', 'penguin', '500'],
-                readOnly: false
-            };
+    // async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+    //     await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    //     console.log(this.roundIndex);
+    //     console.log(this.roundArguments);
+    //     for (let i = 0; i < this.roundArguments.assets; i++) {
+    //         const assetID = `${this.workerIndex}_${i}`;
+    //         console.log(`Worker ${this.workerIndex}: Creating asset ${assetID}`);
+    //         const request = {
+    //             contractId: this.roundArguments.contractId,
+    //             contractFunction: 'CreateAsset',
+    //             invokerIdentity: 'client0.org1.example.com',
+    //             contractArguments: [assetID, 'blue', '20', 'penguin', '500'],
+    //             readOnly: false
+    //         };
 
-            await this.sutAdapter.sendRequests(request);
-        }
-    }
+    //         await this.sutAdapter.sendRequests(request);
+    //     }
+    // }
 
     async submitTransaction() {
         const randomId = Math.floor(Math.random() * this.roundArguments.assets);
@@ -32,8 +32,8 @@ class MyWorkload extends WorkloadModuleBase {
             contractId: this.roundArguments.contractId,
             contractFunction: 'ReadAsset',
             invokerIdentity: 'client0.org1.example.com',
-            // contractArguments: ['asset1'],
-            contractArguments: [`${this.workerIndex}_${randomId}`],
+            contractArguments: ['asset1'],
+            // contractArguments: [`${this.workerIndex}_${randomId}`],
             // targetOrganizations: ["Org1", "Org2"],
             readOnly: true
         };
@@ -42,21 +42,21 @@ class MyWorkload extends WorkloadModuleBase {
 
 
 
-    async cleanupWorkloadModule() {
-        for (let i = 0; i < this.roundArguments.assets; i++) {
-            const assetID = `${this.workerIndex}_${i}`;
-            console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
-            const request = {
-                contractId: this.roundArguments.contractId,
-                contractFunction: 'DeleteAsset',
-                invokerIdentity: 'client0.org1.example.com',
-                contractArguments: [assetID],
-                readOnly: false
-            };
+    // async cleanupWorkloadModule() {
+    //     for (let i = 0; i < this.roundArguments.assets; i++) {
+    //         const assetID = `${this.workerIndex}_${i}`;
+    //         console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
+    //         const request = {
+    //             contractId: this.roundArguments.contractId,
+    //             contractFunction: 'DeleteAsset',
+    //             invokerIdentity: 'client0.org1.example.com',
+    //             contractArguments: [assetID],
+    //             readOnly: false
+    //         };
 
-            await this.sutAdapter.sendRequests(request);
-        }
-    }
+    //         await this.sutAdapter.sendRequests(request);
+    //     }
+    // }
 }
 
 function createWorkloadModule() {
