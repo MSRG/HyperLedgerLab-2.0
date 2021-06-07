77576e865bec26d5d506bdb7154d7c7daae25884
karimabbes <karim.abbes@tum.de>
2021-06-07 11:42:59 +0200

    rgr

--- hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
+++ hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
@@ -33,6 +33,7 @@ class MyWorkload extends WorkloadModuleBase {
             contractFunction: 'ReadAsset',
             invokerIdentity: 'client0.org1.example.com',
             contractArguments: ['asset1'],
+            timeout: 120,
             // contractArguments: [`${this.workerIndex}_${randomId}`],
             // targetOrganizations: ["Org1", "Org2"],
             readOnly: true
