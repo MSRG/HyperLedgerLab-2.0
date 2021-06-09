562952fd1721c95048852e2dadd32d83b308c936
Karim <karim.abbes@tum.de>
2021-06-09 13:29:08 +0200

    df

--- hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
+++ hyperledgerCaliper/asset-transfer-basic/workload/readAsset.js
@@ -11,7 +11,9 @@ class MyWorkload extends WorkloadModuleBase {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
         console.log(this.roundIndex);
         console.log(this.roundArguments);
+        console.log('start');
         for (let i = 0; i < this.roundArguments.assets; i++) {
+            console.log('for loop');
             const assetID = `${this.workerIndex}_${i}`;
             console.log(`Worker ${this.workerIndex}: Creating asset ${assetID}`);
             const request = {
@@ -27,36 +29,36 @@ class MyWorkload extends WorkloadModuleBase {
     }
 
     async submitTransaction() {
-        const randomId = Math.floor(Math.random() * this.roundArguments.assets);
-        const myArgs = {
-            contractId: this.roundArguments.contractId,
-            contractFunction: 'ReadAsset',
-            invokerIdentity: 'client0.org1.example.com',
-            // contractArguments: ['asset1'],
-            timeout: 30,
-            contractArguments: [`${this.workerIndex}_${randomId}`],
-            // targetOrganizations: ["Org1", "Org2"],
-            readOnly: true
-        };
-        await this.sutAdapter.sendRequests(myArgs);
+        // const randomId = Math.floor(Math.random() * this.roundArguments.assets);
+        // const myArgs = {
+        //     contractId: this.roundArguments.contractId,
+        //     contractFunction: 'ReadAsset',
+        //     invokerIdentity: 'client0.org1.example.com',
+        //     // contractArguments: ['asset1'],
+        //     timeout: 30,
+        //     contractArguments: [`${this.workerIndex}_${randomId}`],
+        //     // targetOrganizations: ["Org1", "Org2"],
+        //     readOnly: true
+        // };
+        // await this.sutAdapter.sendRequests(myArgs);
     }
 
 
 
     async cleanupWorkloadModule() {
-        for (let i = 0; i < this.roundArguments.assets; i++) {
-            const assetID = `${this.workerIndex}_${i}`;
-            console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
-            const request = {
-                contractId: this.roundArguments.contractId,
-                contractFunction: 'DeleteAsset',
-                invokerIdentity: 'client0.org1.example.com',
-                contractArguments: [assetID],
-                readOnly: false
-            };
+        // for (let i = 0; i < this.roundArguments.assets; i++) {
+        //     const assetID = `${this.workerIndex}_${i}`;
+        //     console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
+        //     const request = {
+        //         contractId: this.roundArguments.contractId,
+        //         contractFunction: 'DeleteAsset',
+        //         invokerIdentity: 'client0.org1.example.com',
+        //         contractArguments: [assetID],
+        //         readOnly: false
+        //     };
 
-            await this.sutAdapter.sendRequests(request);
-        }
+        //     await this.sutAdapter.sendRequests(request);
+        // }
     }
 }
 
