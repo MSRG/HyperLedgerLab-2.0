6435e724714bc53914928d5b1ab3de24f9ecad0e
Karim <karim.abbes@tum.de>
2021-06-15 18:18:37 +0200

    edits terraform

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -8,35 +8,15 @@ variable "az_list_node" {
   type = list(string)
 }
 
-variable "number_of_k8s_masters" {}
-
-variable "number_of_k8s_masters_no_etcd" {}
-
-variable "number_of_etcd" {}
-
 variable "number_of_k8s_masters_no_floating_ip" {}
-
-variable "number_of_k8s_masters_no_floating_ip_no_etcd" {}
 
 variable "number_of_k8s_nodes" {}
 
 variable "number_of_k8s_nodes_no_floating_ip" {}
 
-variable "number_of_bastions" {}
-
-variable "number_of_gfs_nodes_no_floating_ip" {}
-
-variable "bastion_root_volume_size_in_gb" {}
-
-variable "etcd_root_volume_size_in_gb" {}
-
 variable "master_root_volume_size_in_gb" {}
 
 variable "node_root_volume_size_in_gb" {}
-
-variable "gfs_root_volume_size_in_gb" {}
-
-variable "gfs_volume_size_in_gb" {}
 
 variable "master_volume_type" {}
 
@@ -44,67 +24,43 @@ variable "public_key_path" {}
 
 variable "image" {}
 
-variable "image_gfs" {}
-
 variable "ssh_user" {}
-
-variable "ssh_user_gfs" {}
 
 variable "flavor_k8s_master" {}
 
 variable "flavor_k8s_node" {}
 
-variable "flavor_etcd" {}
+variable "flavor_k8s_master_name" {}
 
-variable "flavor_gfs_node" {}
+variable "flavor_k8s_node_name" {}
 
 variable "network_name" {}
-
-variable "flavor_bastion" {}
 
 variable "network_id" {
   default = ""
 }
 
-variable "k8s_master_fips" {
-  type = list
-}
-
-variable "k8s_master_no_etcd_fips" {
-  type = list
-}
-
-variable "k8s_node_fips" {
-  type = list
-}
-
 variable "k8s_nodes_fips" {
-  type = map
+  type = map(any)
 }
 
 variable "bastion_fips" {
-  type = list
-}
-
-variable "bastion_allowed_remote_ips" {
-  type = list
+  type = list(any)
 }
 
 variable "master_allowed_remote_ips" {
-  type = list
+  type = list(any)
 }
 
 variable "k8s_allowed_remote_ips" {
-  type = list
+  type = list(any)
 }
 
 variable "k8s_allowed_egress_ips" {
-  type = list
+  type = list(any)
 }
 
 variable "k8s_nodes" {}
-
-variable "wait_for_floatingip" {}
 
 variable "supplementary_master_groups" {
   default = ""
@@ -115,11 +71,11 @@ variable "supplementary_node_groups" {
 }
 
 variable "master_allowed_ports" {
-  type = list
+  type = list(any)
 }
 
 variable "worker_allowed_ports" {
-  type = list
+  type = list(any)
 }
 
 variable "use_access_ip" {}
@@ -130,8 +86,4 @@ variable "use_server_groups" {
 
 variable "extra_sec_groups" {
   type = bool
-}
-
-variable "extra_sec_groups_name" {
-  type = string
 }
