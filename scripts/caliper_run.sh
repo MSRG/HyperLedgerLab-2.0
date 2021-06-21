a1b72021ce1748ccfd79f606d1a38a31402d1f2c
Karim <karim.abbes@tum.de>
2021-06-21 14:21:15 +0200

    dfg

--- scripts/caliper_run.sh
+++ scripts/caliper_run.sh
@@ -2,25 +2,21 @@
 set -x
 
 if [ $# -ne 2 ] ; then
-    echo "usage: caliper_run.sh <chaincode_name> <network_folder_name>s" 
+    echo "usage: caliper_run.sh <chaincode_name>" 
     exit 2
 fi
 
 CHAINCODE_NAME="$1"
-NETWORK_FOLDER_NAME="$2"
 
 # Go to hyperledgerCaliper folder
 cd `dirname $0`/../hyperledgerCaliper
 
 if [ ! -d  $CHAINCODE_NAME ] ; then
-    echo "Invalid chaincode folder name"
+    echo "ERROR: Invalid chaincode folder name"
     exit 0
 fi
 
-if [ ! -d networks/$NETWORK_FOLDER_NAME ] ; then
-    echo "Invalid networkConfig folder name"
-    exit 0
-fi
+helm template config-template/ -f ../hyperledgerFabric/network-configuation.yaml --output-dir .
 
 kubectl apply -f mosquitto/
 
@@ -28,14 +24,14 @@ kubectl create configmap benchmarks --from-file=./$CHAINCODE_NAME/benchmarks/
 
 kubectl create configmap workload --from-file=./$CHAINCODE_NAME/workload/
 
-kubectl create configmap network --from-file=./networks/$NETWORK_FOLDER_NAME
+kubectl create configmap network --from-file=./caliper-config/templates/networkConfig.yaml
 
-kubectl create configmap caliper-config --from-file=./caliper.yaml
+kubectl create configmap caliper-config --from-file=./caliper-config/templates/caliper.yaml
 
 kubectl create configmap caliper-report-git --from-file=./git.yaml
 
-kubectl apply -f caliper-deployment.yaml 
+kubectl apply -f caliper-config/templates/caliper-deployment.yaml 
 
-kubectl apply -f caliper-deployment-worker.yaml 
+kubectl apply -f caliper-config/templates/caliper-deployment-worker.yaml 
 
 set +x
